67a2f6887fc01bbfcf809a42c3d6e5a0a42b310b
kubrasayitass <145451189+kubrasayitass@users.noreply.github.com>
2023-10-23 13:01:07 +0300

    Add files via upload

---
+++ for döngüsü ornek2/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace for_döngüsü_ornek2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGoster = new System.Windows.Forms.Button();
+            this.lbSayiler = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnGoster
+            // 
+            this.btnGoster.Location = new System.Drawing.Point(31, 26);
+            this.btnGoster.Name = "btnGoster";
+            this.btnGoster.Size = new System.Drawing.Size(126, 61);
+            this.btnGoster.TabIndex = 0;
+            this.btnGoster.Text = "göster";
+            this.btnGoster.UseVisualStyleBackColor = true;
+            this.btnGoster.Click += new System.EventHandler(this.btnGoster_Click);
+            // 
+            // lbSayiler
+            // 
+            this.lbSayiler.FormattingEnabled = true;
+            this.lbSayiler.Location = new System.Drawing.Point(163, 26);
+            this.lbSayiler.Name = "lbSayiler";
+            this.lbSayiler.Size = new System.Drawing.Size(154, 251);
+            this.lbSayiler.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbSayiler);
+            this.Controls.Add(this.btnGoster);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGoster;
+        private System.Windows.Forms.ListBox lbSayiler;
+    }
+}
+
